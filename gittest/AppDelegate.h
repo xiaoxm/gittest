02e8b68b3b69ae3571bf7e7a82b03ab26989dd29
herui <heruifs@163.com>
2017-05-14 04:34:10 +0800

    xxx

---
+++ gittest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittest
+//
+//  Created by 贺瑞 on 2017/5/14.
+//  Copyright © 2017年 herui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  gittest
+//
+//  Created by 贺瑞 on 2017/5/14.
+//  Copyright © 2017年 herui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
